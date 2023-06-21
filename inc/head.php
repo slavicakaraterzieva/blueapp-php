ec1a3ab7f3f378421e9a2dbada7348ee92560960
slavicakaraterzieva <81101997+slavicakaraterzieva@users.noreply.github.com>
2023-06-21 16:09:14 +0200

    Update head.php

--- inc/head.php
+++ inc/head.php
@@ -14,43 +14,5 @@ require_once ('bootstrap.php');
     <link rel="stylesheet" type="text/css" href="./sass/vendors/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="./sass/vendors/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="./css/style.css">
- 
-    <title>The Blue App</title>
-=======
-<?php
-require_once ('bootstrap.php');
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA Compatible" content="ie-edge">
-    <link rel="stylesheet" type="text/css" href="./sass/vendors/font-awesome/fontawesome-free/css/all.css" >
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&family=Nunito:wght@200;300;400&family=Oxygen:wght@300;400&family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="./sass/vendors/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="./sass/vendors/bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="./css/style.css">
-    <title>The Blue App</title>
->>>>>>> 8909e8750af05f609e80253435cbd9efeec32e28
-=======
-<?php
-require_once ('bootstrap.php');
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA Compatible" content="ie-edge">
-    <link rel="stylesheet" type="text/css" href="./sass/vendors/font-awesome/fontawesome-free/css/all.css" >
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400&family=Nunito:wght@200;300;400&family=Oxygen:wght@300;400&family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="./sass/vendors/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="./sass/vendors/bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="./css/style.css">
     <title>The Blue App</title>
 </head>
